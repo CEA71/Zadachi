1d5b7ae5d7b9b8bd7a2c5142b507517882defcd5
Chesnokov Egor <9036906946@mail.ru>
2022-07-19 20:06:03 +0300

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Egor");
+//        System.out.println("Hello, Egor");
         JOptionPane.showMessageDialog(null, "Hello, Egor!");
     }
 }
