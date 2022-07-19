dd6a3128e66ee4e8865c65b2f7d55f44669d9d3e
Chesnokov Egor <9036906946@mail.ru>
2022-07-19 19:28:53 +0300

    first

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Egor");
+        JOptionPane.showMessageDialog(null, "Hello, Egor!");
+    }
+}
