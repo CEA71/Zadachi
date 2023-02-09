27a747a58ae258f95b0f3a427093942939e2f9b2
CEA71 <e.ches71@ya.ru>
2023-02-09 16:01:46 +0300

    Tank modern

--- src/Tank/Main.java
+++ src/Tank/Main.java
@@ -6,17 +6,26 @@ public class Main {
 Make a Tank(add code to class Tank),
 which can move forward and backward
 */
+        // создаем танк At (0;0) fuel=100
+        Tank justTank = new Tank();
+//        justTank.goForward(10);
+//        justTank.printPosition();
+//        justTank.turnRight();
+//        justTank.goForward(50);
+//        justTank.printPosition();
+//        justTank.turnLeft();
+//        justTank.goBackward(100);
+//        justTank.printPosition();
+        // создаем танк At (10;10) fuel=100
+        Tank anywareTank = new Tank(10, 10);
+        // создаем танк At (20;30) fuel=200
+        Tank customTank = new Tank(20, 30, 200);
 
-        Tank tank = new Tank();
-        tank.goForward(10);
-        tank.printPosition();
-        tank.turnRight();
-        tank.goForward(50);
-        tank.printPosition();
-        tank.turnLeft();
-        tank.goBackward(100);
-        tank.printPosition();
-
-
+        justTank.goForward(200);
+        justTank.printPosition();
+        anywareTank.goBackward(-200);
+        anywareTank.printPosition();
+        customTank.goForward(201);
+        customTank.printPosition();
     }
 }
--- src/Tank/Tank.java
+++ src/Tank/Tank.java
@@ -3,46 +3,82 @@ package Tank;
 public class Tank {
     int position_x = 0;
     int position_y = 0;
+    int fuel = 100;
     // задаем начальное направление. Танк смотрит от центра по оси Х в плюсовые значения
     int xy = 1;
+    //конструктор 1
+    public Tank() {
+    }
+    //конструктор 2
+    public Tank(int position_x, int position_y, int fuel) {
+        this.position_x = position_x;
+        this.position_y = position_y;
+        this.fuel = fuel;
+    }
+    //конструктор 3
+    public Tank(int position_x, int position_y) {
+        this.position_x = position_x;
+        this.position_y = position_y;
+    }
     // направление 3 - это в минусовые значения по оси Х
     // направление 2 - это в плюсовые значения по оси Y
     // направление 4 - это в минусовые значения по оси Y
 
     public void goForward(int position){
+        // проверка: достаточно ли топлива для совершения манёвра
+        if (fuel - position >= 0){
+            position = position;
+        } else
+            position = fuel;
+
         switch (xy) {
             case 1:
                 this.position_x = position_x + position;
+                fuel -= position;
                 break;
             case 3:
                 this.position_x = position_x - position;
+                fuel -= position;
                 break;
             case 2:
                 this.position_y = position_y + position;
+                fuel -= position;
                 break;
             case 4:
                 this.position_y = position_y - position;
+                fuel -= position;
                 break;
             default:
         }
     }
     public void goBackward(int position){
+        // проверка: достаточно ли топлива для совершения манёвра
+        if (fuel - position >= 0){
+            position = position;
+        } else
+            position = fuel;
+
         switch (xy) {
             case 1:
                 this.position_x = position_x - position;
+                fuel -= position;
                 break;
             case 3:
                 this.position_x = position_x + position;
+                fuel -= position;
                 break;
             case 2:
                 this.position_y = position_y - position;
+                fuel -= position;
                 break;
             case 4:
                 this.position_y = position_y + position;
+                fuel -= position;
                 break;
             default:
         }
     }
+    // реализация поворота направо
     public void turnRight(){
         if (xy == 1){
             xy = 2;
@@ -54,6 +90,7 @@ public class Tank {
             xy = 1;
         }
     }
+    //реализация поворота налево
     public void turnLeft(){
         if (xy == 1){
             xy = 4;
@@ -68,7 +105,5 @@ public class Tank {
     public void printPosition(){
         System.out.println("The Tank is at " + position_x + ", " + position_y + " now");
     }
-//    public void goBackward(int position){
-//        this.position -= position;
-//    }
+
 }
